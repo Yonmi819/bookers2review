7e97dc4fa697d9e90e7f948dbbffd455a0fdcb92
EmiSuzuki <mush.2525miumiu@gmail.com>
2022-12-28 02:53:42 +0000

    [Update]ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   
   get 'home/about' => "homes#about", as: "about"
-  resources :books, only:[:new, :index, :show, :edit, :create, :update, :destroy]
+  resources :books, only:[:new, :index, :show, :edit, :create, :update, :destroy] do
+    resources :book_comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
+  end
   resources :users, only:[:index, :show, :edit, :update]
   
 
